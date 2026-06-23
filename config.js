48226a50e7d2c2313ffe8b9d760dbf4437570e8f
chj927il <chj927il@etoos.com>
2026-06-23 10:20:28 +0900

    대성디랩 추가

--- config.js
+++ config.js
@@ -9,6 +9,15 @@ module.exports = {
       ],
       focusSelectors: ['h1', 'h2', 'h3', '.banner', '.promotion', '.event', '.notice', 'p', '.title', '.content'],
     },
+    {
+      id: 'daesung_dlab',
+      name: '대성디랩',
+      url: 'https://dlab.dsdo.co.kr',
+      eventUrls: [
+        'https://dlab.dsdo.co.kr/about/events',
+      ],
+      focusSelectors: ['h1', 'h2', 'h3', '.event', '.notice', 'p', '.title', '.content'],
+    },
   ],
   geminiModel: 'gemini-2.5-flash',
   crawlSchedule: '0 15 * * 5',
